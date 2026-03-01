8993e20dbab39efd0ab2cc188affc88c256fed94
nirtisum-cs <nirtisum.cs@gmail.com>
2026-03-02 02:24:48 +0600

    Add String Input and output

---
+++ 1_Intro_to_C++/stringIO.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main ()
+{
+    char s[100]; cin >> s; cout << s << endl;
+
+    string str; getline(cin, str); cout << str << endl;
+
+    char myStr[100]; cin.getline(myStr,100); cout << myStr << endl;
+
+    return 0;
+}
